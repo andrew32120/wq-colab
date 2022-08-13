beeb36f9f08d7495e471c917081f1f5de782be35
andrew32120 <96460046+andrew32120@users.noreply.github.com>
2022-08-13 11:12:59 +0000

    Update tes786.sh

--- tes786.sh
+++ tes786.sh
@@ -1,6 +1,6 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-./ngrok authtoken 1ocNu9P9mlGOHr5ZB0v3k9ZqD6H_6WK76KEyg3MtwRHohtr7F" > /root/.ngrok2/ngrok.yml 
+./ngrok authtoken 1ocNu9P9mlGOHr5ZB0v3k9ZqD6H_6WK76KEyg3MtwRHohtr7F > /root/.ngrok2/ngrok.yml 
 nohup ./ngrok tcp 3388 &>/dev/null &
 ./ngrok tcp 3388 &>/dev/null &
 echo Please wait for installing...
